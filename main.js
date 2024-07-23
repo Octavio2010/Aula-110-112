f97cf091451b9b47cc9691c2ff2f91c7ee453b4a
Octavio2010 <131196314+Octavio2010@users.noreply.github.com>
2024-07-23 15:34:04 -0300

    Aula 112(Final)

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ function takeSnapshot(){
     Webcam.snap(function(data_uri){
         document.getElementById("result").innerHTML='<img id = "Capturada" src = "'+data_uri+'"/>'
     })
-    speak()
+    
 }
 
 console.log("ml5_version",ml5.version)
@@ -32,4 +32,51 @@ function speak(){
     var  speakData2 = "A segunda previsão é" + prediction_2
     var utterThis = new SpeechSynthesisUtterance(speakData1+speakData2)
     synth.speak(utterThis)
+}
+function Check(){
+    img = document.getElementById("Capturada")
+    classifier.classify(img,gotResult)
+}
+
+function gotResult(error,results){
+    if(error){
+        console.error(error)
+    }
+    else{
+        console.log(results)
+        document.getElementById("ResultEmotionName").innerHTML=results[0].label
+        document.getElementById("ResultEmotionName2").innerHTML=results[1].label
+
+        prediction_1=results[0].label
+        prediction_2=results[1].label
+
+        speak()
+
+        if(results[0].label=="feliz"){
+            document.getElementById("UpdateEmoji").innerHTML="&#128522"
+        }
+        if(results[0].label=="triste"){
+            document.getElementById("UpdateEmoji").innerHTML="&#128532"
+        }
+        if(results[0].label=="chorando"){
+            document.getElementById("UpdateEmoji").innerHTML="&#128546"
+        }
+        if(results[0].label=="bravo"){
+            document.getElementById("UpdateEmoji").innerHTML="&#128545"
+        }
+
+        
+        if(results[1].label=="feliz"){
+            document.getElementById("UpdateEmoji2").innerHTML="&#128522"
+        }
+        if(results[1].label=="triste"){
+            document.getElementById("UpdateEmoji2").innerHTML="&#128532"
+        }
+        if(results[1].label=="chorando"){
+            document.getElementById("UpdateEmoji2").innerHTML="&#128546"
+        }
+        if(results[1].label=="bravo"){
+            document.getElementById("UpdateEmoji2").innerHTML="&#128545"
+        }
+    }
 }
